55a709ddc7a1c57bbb18af30580ffef26e0932d7
QGBALTIGO <119024696+QGBALTIGO@users.noreply.github.com>
2026-09-22 17:56:16 -0400

    Add MTProto speed smoke setting

--- app/settings.py
+++ app/settings.py
@@ -47,6 +47,7 @@ class Settings:
     run_video_smoke: bool = _bool("IRIS_RUN_VIDEO_SMOKE", False)
     run_large_video_smoke: bool = _bool("IRIS_RUN_LARGE_VIDEO_SMOKE", False)
     run_source_speed_smoke: bool = _bool("IRIS_RUN_SOURCE_SPEED_SMOKE", False)
+    run_mtproto_speed_smoke: bool = _bool("IRIS_RUN_MTPROTO_SPEED_SMOKE", False)
 
 
 settings = Settings()
